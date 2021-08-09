7a31f3724a37ab224a51bf0af69c82e4f24f9f33
wy <wy@wy.com>
2021-08-09 20:06:02 +0800

    init

---
+++ sigtest.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/rockiecn/test-sig/sig/implement/sigapi"
+	"github.com/rockiecn/test-sig/sig/implement/utils"
+)
+
+func main() {
+	var msg = []byte("hello") // msg to be signed
+	var signed = false        // flag
+	var sigByte []byte        // store signature
+	var skHex string          // for sk input
+
+	for {
+		var cmd string
+
+		fmt.Println(">> Intput cmd, 1 to sign, 2 to verify, 3 to clear signature")
+		fmt.Scanf("%s", &cmd)
+
+		// decode address , Hex string to []byte
+		fromAddrByte, err := hex.DecodeString("9e0153496067C20943724b79515472195A7aEDAa")
+		if err != nil {
+			log.Fatal("decode err.")
+			return
+		}
+		// []byte to common.Address
+		fromAddress := common.BytesToAddress(fromAddrByte)
+		fmt.Println("fromAddress:", fromAddress)
+
+		switch cmd {
+		case "1":
+			if signed {
+				fmt.Println("already signed, run 3 to clear signature first")
+				continue
+			}
+
+			fmt.Println("Input private key:")
+			fmt.Scanf("%s", &skHex)
+			// string to byte
+			skByte := utils.Str2Byte(skHex)
+			// call sign
+			sigByte, err = sigapi.Sign(msg, skByte)
+			if err != nil {
+				log.Println("sign err:", err)
+				continue
+			}
+
+			signed = true
+
+			// bytes to string
+			fmt.Println("signature:", hexutil.Encode(sigByte)) //
+
+		case "2":
+			if signed {
+				sigapi.Verify(msg, sigByte, fromAddress)
+				continue
+			} else {
+				fmt.Println("not signed yet, run 1 to signed first")
+				continue
+			}
+
+		case "3":
+			signed = false
+			continue
+		}
+	}
+
+}
